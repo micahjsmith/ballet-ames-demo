71771fd72e975d95d898f8d47712f60e024d4ee6
Micah Smith <micahs@mit.edu>
2019-03-06 14:50:04 -0500

    Load ames data from s3

--- ames/load_data.py
+++ ames/load_data.py
@@ -1,3 +1,18 @@
+import pandas as pd
+
+
 def load_data(input_dir=None):
-    """Load data"""
-    raise NotImplementedError
+    """Loads the Ames Housing dataset
+
+    Source:
+
+        Decock, Dean. "Ames, Iowa: Alternative to the Boston Housing Data as an
+        End of Semester Regression Project."
+        <https://ww2.amstat.org/publications/jse/v19n3/decock.pdf>
+    """
+    source = 'https://s3.amazonaws.com/mit-dai-ballet/ames/AmesHousing.txt'
+    df = pd.read_table(source)
+    X = df.drop('SalePrice', axis=1)
+    y = df['SalePrice']
+    return X, y
+
