17ae9008795df85b9d1ac5c545852604c8727a02
Micah Smith <micahs@mit.edu>
2019-03-27 15:23:12 -0400

    Update load_data

--- ames/load_data.py
+++ ames/load_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from ballet.util import one_or_raise
 from ballet.util.io import load_table_from_config
-from funcy import some, where
+from funcy import lwhere
 
 from ames import conf
 
@@ -19,16 +20,16 @@ def load_data(input_dir=None):
         tables = conf.get('tables')
 
         entities_table_name = conf.get('data', 'entities_table_name')
-        entities_config = some(where(tables, name=entities_table_name))
-        X = load_table_from_config(input_dir, entities_config)
+        entities_config = one_or_raise(lwhere(tables, name=entities_table_name))
+        X_df = load_table_from_config(input_dir, entities_config)
 
         targets_table_name = conf.get('data', 'targets_table_name')
-        targets_config = some(where(tables, name=targets_table_name))
-        y = load_table_from_config(input_dir, targets_config)
+        targets_config = one_or_raise(lwhere(tables, name=targets_table_name))
+        y_df = load_table_from_config(input_dir, targets_config)
     else:
         source = 'https://s3.amazonaws.com/mit-dai-ballet/ames/AmesHousing.txt'
         df = pd.read_csv(source, sep='\t')
-        X = df.drop('SalePrice', axis=1)
-        y = df['SalePrice']
+        X_df = df.drop('SalePrice', axis=1)
+        y_df = df['SalePrice']
 
-    return X, y
+    return X_df, y_df
