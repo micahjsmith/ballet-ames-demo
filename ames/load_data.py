6da0f8be9c14fc1ea1d505f2492e0320b91ae295
Micah Smith <micahs@mit.edu>
2019-03-27 15:23:20 -0400

    Add make_train_test_split

--- ames/load_data.py
+++ ames/load_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from ballet.compat import pathlib
 from ballet.util import one_or_raise
 from ballet.util.io import load_table_from_config
 from funcy import lwhere
+from sklearn.model_selection import train_test_split
 
 from ames import conf
 
@@ -33,3 +35,42 @@ def load_data(input_dir=None):
         y_df = df['SalePrice']
 
     return X_df, y_df
+
+
+def make_train_test_split(output_dir, seed=641137):
+    # load and split data
+    X, y = load_data()
+    inds = X.index.copy()
+    inds_tr, inds_te = train_test_split(inds,
+                                        train_size=0.67,
+                                        test_size=0.33,
+                                        random_state=seed)
+    X_tr_df = X.loc[inds_tr]
+    X_te_df = X.loc[inds_te]
+    y_tr_df = y.loc[inds_tr]
+    y_te_df = y.loc[inds_te]
+
+    # load config
+    tables = conf.get('tables')
+    entities_table_name = conf.get('data', 'entities_table_name')
+    entities_config = one_or_raise(lwhere(tables, name=entities_table_name))
+    entities_path = entities_config['path']
+    targets_table_name = conf.get('data', 'targets_table_name')
+    targets_config = one_or_raise(lwhere(tables, name=targets_table_name))
+    targets_path = targets_config['path']
+
+    # prepare directories
+    output_dir = pathlib.Path(output_dir)
+    train_dir = output_dir.joinpath('train')
+    train_dir.mkdir(exist_ok=True)
+    test_dir = output_dir.joinpath('test')
+    test_dir.mkdir(exist_ok=True)
+
+    # save tables
+    kwargs = {'header': True}
+    X_tr_df.to_csv(train_dir.joinpath(entities_path), **kwargs)
+    X_te_df.to_csv(test_dir.joinpath(entities_path), **kwargs)
+    y_tr_df.to_csv(train_dir.joinpath(targets_path), **kwargs)
+    y_te_df.to_csv(test_dir.joinpath(targets_path), **kwargs)
+
+    return X_tr_df, X_te_df, y_tr_df, y_te_df
