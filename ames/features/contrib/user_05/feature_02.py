4f680cbb40a92a68d76376fb4dc2979f11f5c2b3
kelvin-lu <kelvinlu@mit.edu>
2019-04-18 14:27:44 -0400

    Add new feature

---
+++ ames/features/contrib/user_05/feature_02.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["MS Zoning"]
+transformer = [SimpleImputer(strategy="most_frequent"), OneHotEncoder()]
+name = "MS Zoning Fill"
+feature = Feature(input=input, transformer=transformer, name=name)
