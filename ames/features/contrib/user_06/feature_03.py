02ad88ec56bf2417509a7c01d283431a64816650
kelvin-lu <kelvinlu@mit.edu>
2019-04-22 22:25:23 -0400

    Add new feature

---
+++ ames/features/contrib/user_06/feature_03.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from ballet.eng import NullFiller
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["Garage Type"]
+transformer = [NullFiller(replacement="Missing"), OneHotEncoder()]
+name = "Garage Type Fill"
+feature = Feature(input=input, transformer=transformer, name=name)
