f4da3c4cadf367e85e4f8563d4f9b7e516a3adac
kelvin-lu <kelvinlu@mit.edu>
2019-04-22 21:16:50 -0400

    Add new feature

---
+++ ames/features/contrib/user_09/feature_02.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from ballet.eng import NullFiller
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["Bsmt Qual"]
+transformer = [NullFiller(replacement="None"), OneHotEncoder()]
+name = "Basement Height Fill"
+feature = Feature(input=input, transformer=transformer, name=name)
