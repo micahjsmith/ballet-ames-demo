a00b43534b0d409299f66a489e48d296122e6819
kelvin-lu <kelvinlu@mit.edu>
2019-04-23 03:52:18 -0400

    Prune redundant feature: User 6, Feature 4

--- ames/features/contrib/user_06/feature_04.py
+++
@@ -1,8 +0,0 @@
-from ballet import Feature
-from ballet.eng import NullFiller
-from sklearn.preprocessing import OneHotEncoder
-
-input = ["Garage Finish"]
-transformer = [NullFiller(replacement="Missing"), OneHotEncoder()]
-name = "Garage Finish Fill"
-feature = Feature(input=input, transformer=transformer, name=name)
