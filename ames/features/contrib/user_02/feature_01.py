bee2f57b79d0a0c65d4268c762cb614ff74e941f
kelvin-lu <kelvinlu@mit.edu>
2019-04-22 21:08:22 -0400

    Prune redundant feature: User 2, Feature 1

--- ames/features/contrib/user_02/feature_01.py
+++
@@ -1,8 +0,0 @@
-from ballet import Feature
-from sklearn.preprocessing import OneHotEncoder
-
-
-input = ["Yr Sold"]
-transformer = OneHotEncoder()
-name = "Year Sold Categorical"
-feature = Feature(input=input, transformer=transformer, name=name)
