5d86163619fa828b53d45ee451e2dd5f8ce7efb4
kelvin-lu <kelvinlu@mit.edu>
2019-04-22 22:13:01 -0400

    Add new feature

---
+++ ames/features/contrib/user_01/feature_03.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from ballet.eng import NullFiller
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["Misc Feature"]
+transformer = [NullFiller(replacement="None"), OneHotEncoder()]
+name = "Misc Housing Feature Type"
+feature = Feature(input=input, transformer=transformer, name=name)
