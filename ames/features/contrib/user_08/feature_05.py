2010087abb38efee257900ae4ee4d6bd4ba633ba
kelvin-lu <kelvinlu@mit.edu>
2019-04-23 03:54:22 -0400

    Add new feature

---
+++ ames/features/contrib/user_08/feature_05.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["Electrical"]
+transformer = [SimpleImputer(strategy="mean"), OneHotEncoder()]
+name = "Electrical Type Fill"
+feature = Feature(input=input, transformer=transformer, name=name)
