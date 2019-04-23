55a8a36a19816349fd37d24adc811101d0461156
kelvin-lu <kelvinlu@mit.edu>
2019-04-22 21:47:48 -0400

    Prune redundant feature: User 8, Feature 1

--- ames/features/contrib/user_08/feature_01.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from ballet import Feature
-from ballet.eng import SimpleFunctionTransformer
-
-input = "Lot Area"
-transformer = SimpleFunctionTransformer(np.sqrt)
-name = "Sqrt Lot Area"
-feature = Feature(input=input, transformer=transformer, name=name)
