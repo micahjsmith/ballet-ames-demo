62d31fdb6b1e7720bd95b3d2601ee426d6f98742
kelvin-lu <kelvinlu@mit.edu>
2019-04-22 22:13:53 -0400

    Add new feature

---
+++ ames/features/contrib/user_05/feature_03.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from ballet.eng import NullFiller
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["MS SubClass"]
+transformer = [NullFiller(replacement="None"), OneHotEncoder()]
+name = "MS Fill None"
+feature = Feature(input=input, transformer=transformer, name=name)
